8f3c1aeb55fb64b15c0fd7f2447eecddd28b4c40
David van Oudheusden <doudheusden@novacollege.nl>
2024-11-19 00:00:09 +0100

    Feature: added none

--- src/Book.js
+++ src/Book.js
@@ -23,6 +23,7 @@ const Book = ({ book, updateShelve }) => (
           <option value="currentlyReading">Currently Reading</option>
           <option value="wantToRead">Want to Read</option>
           <option value="read">Read</option>
+          <option value="none">None</option>
         </select>
       </div>
     </div>
