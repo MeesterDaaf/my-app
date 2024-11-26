29b7b6c0e8391e4bb307c4f544318cb1b4ffbb50
David van Oudheusden <doudheusden@novacollege.nl>
2024-11-26 21:57:24 +0100

    Bug fix: added condition to check if option is selected

--- src/Book.js
+++ src/Book.js
@@ -16,14 +16,27 @@ const Book = ({ book, updateShelve }) => (
 
       <div className="book-shelf-changer">
         <select
-          value={book.shelf}
+          value={book.shelf ? book.shelf : "none"}
           onChange={(event) => updateShelve(book, event.target.value)}
         >
-          <option value="none">Move to...</option>
-          <option value="currentlyReading">Currently Reading</option>
-          <option value="wantToRead">Want to Read</option>
-          <option value="read">Read</option>
-          <option value="none">None</option>
+          <option value="none" disabled>
+            Move to...
+          </option>
+          <option
+            value="currentlyReading"
+            selected={book.shelf === "currentlyReading"}
+          >
+            Currently Reading
+          </option>
+          <option value="wantToRead" selected={book.shelf === "wantToRead"}>
+            Want to Read
+          </option>
+          <option value="read" selected={book.shelf === "read"}>
+            Read
+          </option>
+          {(book.shelf !== "none" || book.shelf == null) && (
+            <option value="none">None</option>
+          )}
         </select>
       </div>
     </div>
